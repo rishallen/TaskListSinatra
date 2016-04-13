4fd7ee2f8f7171850316b5ac4ae17f9b6678835c
Cristal Tay <bluecolorpencils@gmail.com>
2016-04-13 14:52:32 -0700

    figured out how to redirect the user to the homepage via redirect in the tasklist.rb.

--- lib/query.rb
+++ lib/query.rb
@@ -9,10 +9,10 @@ class  TasksQuery
 
   def taskrecord(row)
     db.execute( "INSERT into tasks values (a = :title, b = :description, c = :completed_at )", *bind_vars )
-# hash_of_hashes.each do |key, value_array|
-    # :title => value_array[0]
-    # :description => value_array[1]
-    # :completed_at => value_array[2]
+    # hash_of_hashes.each do |key, value_array|
+      # :title => value_array[0]
+      # :description => value_array[1]
+      # :completed_at => value_array[2]
     #end
   end
 
--- tasklist.rb
+++ tasklist.rb
@@ -10,19 +10,34 @@ class TaskInput < Sinatra::Base
     @current_db ||= TaskList::Database.new
   end
 
+  # Step 5: Browser makes a separate request for the homepage
   get '/' do
     @tasks = current_database.get_tasks
     erb :index
   end
 
+  post '/' do
+    erb :index
+  end
+
+  # Step 1: Browser asks for the form
   get '/task-form' do
     erb :'task-form'
   end
 
+  # Step 2: Browser submits the form with data (which we get in params)
   post '/task-form' do
+    # Step 3: We save that data into the DB
     current_database.create_task(params[:task_list][:title], params[:task_list][:description])
-    erb :index
+
+    # Step 4: We tell the browser that it should go to the homepage
+    redirect '/'
   end
+
+  # post '/index' do
+  #   @tasks = current_database.get_tasks
+  #   erb :index
+  # end
 
     run!
 end
